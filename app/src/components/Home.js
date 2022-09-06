60d466901415354c1000f09d5f6c0bb3fc32d350
trevinalonzo <webdevtrev@protonmail.com>
2022-09-06 10:39:38 -0700

    slider update

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,28 +2,25 @@ import { ProjectCard } from '../cards/ProjectCard'
 import projects from '../context/projects.json'
 import arrow from '../assets/arrow.svg'
 import '../styles/Home.css'
-import { useState, useRef } from 'react'
-import scrollIntoView from 'scroll-into-view'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
 
-  const [rightArrow, setRightArrow] = useState(true);
-  const [rightArrowState, setRightState] = useState("invert(100%)")
-  const [leftArrow, setLeftArrow] = useState(true);
-  const [leftArrowState, setLeftState] = useState("invert(100%)")
-  const [projectPosition, setPosition] = useState(0);
+  let [projectPosition, setPosition] = useState(0);
 
-  const scrollLeft = function(){
-    if(projectPosition < 0) {
-      setPosition(projectPosition + 1)
-      setLeftState(leftArrowState == "invert(100%)")
-    } else if(projectPosition === 0){
-      setLeftState(leftArrowState == "invert(90%)")
-    }
+  const projectCount = projects.length
+
+  const scrollPrevious = () => {
+    const isFirst = projectPosition === 0;
+    const newPosition = isFirst ? projectCount - 1 : projectPosition - 1;
+    setPosition(newPosition);
+    document.getElementById(newPosition).scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
   }
-
-  const scrollRight = function(){
-
+  const scrollNext = () => {
+    const isLast = projectPosition === projectCount - 1;
+    const newPosition = isLast ? 0 : projectPosition + 1;
+    setPosition(newPosition);
+    document.getElementById(newPosition).scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
   }
 
   return (
@@ -34,17 +31,17 @@ export default function Home() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
       <div className='projectsContainer' data-aos="fade-up" data-aos-offset="100">
-        <div className='projectsCarousel'>
-          <>
-            {projects.map((Project) => (
-                <ProjectCard  title={Project.name} id={Project.id} textColor={Project.textColor} description={Project.description} background={Project.background}/>
-            ))}
-          </>
-        </div>
+        <img alt='scroll-left' src={arrow} style={{width: "52px", transform: "scaleX(-1)", filter: "invert(100%)"}} onClick={() =>scrollPrevious()} />
+          <div className='projectsCarousel'>
+            <>
+              {projects.map((Project, i) => (
+                  <ProjectCard key={i} title={Project.name} id={i} textColor={Project.textColor} description={Project.description} background={Project.background}/>
+              ))}
+            </>
+          </div>
+        <img alt='scroll-right' src={arrow} style={{width: "52px", filter: "invert(100%)", alignSelf: "center"}} onClick={() => scrollNext()}/>
       </div>
         <div className='projectsControl'>
-          <img alt='scroll-left' src={arrow} style={{width: "52px", transform: "scaleX(-1)",  filter: leftArrowState}} onClick={() =>scrollLeft()} />
-          <img alt='scroll-right' src={arrow} style={{width: "52px", filter: rightArrowState}} onClick={() => scrollRight()}/>
           <p>position: {projectPosition}</p>
         </div>
     </>
